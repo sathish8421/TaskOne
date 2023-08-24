fc19ee76cad821fca24b1c5f05f6828f9ca2ddf7
sathis8421 <snkumarsathish@gmail.com>
2023-08-24 12:18:31 +0530

    new code

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class Test {
+	private void method() {
+		System.out.println("Java is simple program language");
+
+	}
+	public static void main(String[] args) {
+		Test a=new Test();
+		a.method();
+		
+		
+	}
+
+}
