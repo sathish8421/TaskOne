11bb4dfced33ac39eed2a1abd6e23fb95734b3ad
sathis8421 <snkumarsathish@gmail.com>
2023-08-25 08:51:51 +0530

    mmm

---
+++ Practise2.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class Practise2 {
+	private void method() {
+		System.out.println("Object Oriented Programming Structure");
+
+	}
+	public static void main(String[] args) {
+		Practise2 a=new Practise2();
+		a.method();
+		
+		
+	}
+	
+
+}
